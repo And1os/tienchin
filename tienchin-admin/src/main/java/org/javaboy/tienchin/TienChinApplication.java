53f1347febfc07918b28a5d4f336539ebd7c031b
liu-yulong <lyl1123581321@qq.com>
2023-04-28 19:28:02 +0800

    :tada: 修改启动项多余代码

--- tienchin-admin/src/main/java/org/javaboy/tienchin/TienChinApplication.java
+++ tienchin-admin/src/main/java/org/javaboy/tienchin/TienChinApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TienChinApplication {
     public static void main(String[] args) {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(TienChinApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  TienChin健身启动成功   ლ(´ڡ`ლ)ﾞ");
     }
